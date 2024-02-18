c7b9865e0bad99070db288ce8040d57e82d144d8
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2024-02-18 17:02:44 +0300

    New commit

--- src/test/java/ru/netology/web/TestWebsite.java
+++ src/test/java/ru/netology/web/TestWebsite.java
@@ -4,31 +4,53 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TestWebsite {
     private WebDriver driver;
 
     @BeforeAll
-    static void setUpAll() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+    public static void setup() {
+        WebDriverManager.chromedriver().setup();
     }
 
 
     @BeforeEach
-    void setUp() { driver = new ChromeDriver(); }
+   public void beforeEach() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("");
+        options.addArguments("");
+        options.addArguments("");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+
+    }
 
     @AfterEach
-    void tearDown() {
+    private void afterEach() {
         driver.quit();
         driver = null;
-
-        @Test
-        void shouldTestSomerhing() {
-           // throw new UnsupportedOperationException();
-
-
-        }
     }
+
+    @Test
+    public void shouldBeFailedIncorrectNameInput() {
+        driver.findElement(By.xpath("")).sendKeys();
+        driver.findElement(By.xpath("")).sendKeys();
+        driver.findElement(By.xpath("")).click();
+        driver.findElement(By.xpath("")).click();
+        assertEquals("" ,
+                driver.findElement(By.xpath(""))
+                        .getText().trim();
+    }
+
+
+
 }
+
+
+
