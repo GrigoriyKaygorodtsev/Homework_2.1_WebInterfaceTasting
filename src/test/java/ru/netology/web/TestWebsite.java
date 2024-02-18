6ba6c91bb7009901298420dd452d61926e5ea9d7
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2024-02-18 18:24:14 +0300

    Add testing

--- src/test/java/ru/netology/web/TestWebsite.java
+++ src/test/java/ru/netology/web/TestWebsite.java
@@ -11,21 +11,26 @@ import org.openqa.selenium.chrome.ChromeOptions;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 class TestWebsite {
     private WebDriver driver;
 
     @BeforeAll
-    public static void setup() {
+    public static void setupAll() {
         WebDriverManager.chromedriver().setup();
     }
-
 
     @BeforeEach
    public void beforeEach() {
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("");
-        options.addArguments("");
-        options.addArguments("");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
 
@@ -39,13 +44,15 @@ class TestWebsite {
 
     @Test
     public void shouldBeFailedIncorrectNameInput() {
-        driver.findElement(By.xpath("")).sendKeys();
-        driver.findElement(By.xpath("")).sendKeys();
-        driver.findElement(By.xpath("")).click();
-        driver.findElement(By.xpath("")).click();
-        assertEquals("" ,
-                driver.findElement(By.xpath(""))
-                        .getText().trim();
+        driver.findElement(By.cssSelector("[data-test-id='name']")).sendKeys();
+        driver.findElement(By.cssSelector("[data-test-id='phone']")).sendKeys();
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button.button")).click();
+        var actualTextElement = driver.findElement(By.cssSelector("[data-test-id=order-success]"));
+        var actualText = actualTextElement.getText().trim();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", actualText);
+        //assertTrue(actualTextElement.isDisplayed());
+
     }
 
 
