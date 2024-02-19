0303f8b8f05975177bf7957396b48281d079279b
Grigoriy Kaygorodtsev <grigoriy.kaygorotsev@gmail.com>
2024-02-19 22:05:04 +0300

    Updated build.gradle

--- src/test/java/ru/netology/web/TestWebsite.java
+++ src/test/java/ru/netology/web/TestWebsite.java
@@ -9,14 +9,11 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 
-//import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
